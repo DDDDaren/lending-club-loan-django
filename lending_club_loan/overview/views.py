e39b98b376575ca94ff1bac3326b9aca02c36f1e
DDDDaren <dxz138@case.edu>
2018-07-29 14:12:39 -0400

    edited url and views

--- lending_club_loan/overview/views.py
+++ lending_club_loan/overview/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def home(request):
+	return HttpResponse('<p>home view</p>')
+
+def loan_detail(request, id):
+	return HttpResponse('<p>loan_detail view with id {}</p>'.format(id))
