e3ed061afeb60ecd1295528db4604f320ceb4f2a
DDDDaren <dxz138@case.edu>
2018-07-29 00:40:14 -0400

    fixed a couple of bugs in models

--- lending_club_loan/overview/models.py
+++ lending_club_loan/overview/models.py
@@ -6,12 +6,14 @@ class LoanStats(models.Model):
 	title = models.CharField(max_length=100)
 	description = models.TextField()
 	funded_amount = models.IntegerField()
-	purpose = models.models.TextField()
+	purpose = models.TextField()
 
 class BrowseNotes(models.Model):
-	GRADE_CHOCIES = ['A', 'B', 'C', 'D', 'E', 'F']
-	loan_id  = models.CharField(max_length==100)
+	GRADE_CHOCIES = [('A', 'A'), ('B', 'B'),
+					 ('C', 'C'), ('D', 'D'), ('E', 'E'), 
+					 ('F', 'F')]
+	loan_id  = models.CharField(max_length=100)
 	title = models.CharField(max_length=100)
 	grade = models.CharField(choices=GRADE_CHOCIES, max_length=1, blank=True)
-	purpose = models.models.TextField()
+	purpose = models.TextField()
 	term = models.IntegerField()
