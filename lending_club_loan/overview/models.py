0e27f061a1f6ad75601507eee42f36fd605b1432
DDDDaren <dxz138@case.edu>
2018-07-29 00:30:14 -0400

    fill in part of the data model

--- lending_club_loan/overview/models.py
+++ lending_club_loan/overview/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class LoanStats(models.Model):
+	loan_id = models.CharField(max_length=100)
+	title = models.CharField(max_length=100)
+	description = models.TextField()
+	funded_amount = models.IntegerField()
+	purpose = models.models.TextField()
+
+class BrowseNotes(models.Model):
+	GRADE_CHOCIES = ['A', 'B', 'C', 'D', 'E', 'F']
+	loan_id  = models.CharField(max_length==100)
+	title = models.CharField(max_length=100)
+	grade = models.CharField(choices=GRADE_CHOCIES, max_length=1, blank=True)
+	purpose = models.models.TextField()
+	term = models.IntegerField()
